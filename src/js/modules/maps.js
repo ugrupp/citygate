16bb61ebb1c666d77e60fcb76c7ca55e855c0b59
Urs Grupp <urs.grupp@gmail.com>
2018-06-05 21:35:39 +0200

    Refactored map JS, added travel markers

--- src/js/modules/maps.js
+++ src/js/modules/maps.js
@@ -1,44 +1,45 @@
-class MapModals {
-  constructor() {
-    if (typeof window.google !== 'undefined') {
-      this.modals = [...document.querySelectorAll('[data-map-modal]')];
-      if (this.modals.length) {
-        this.modals.forEach((modal) => {
-          new MapModal(modal);
-        });
-      }
-    }
-  }
-}
+import LOCATIONS from './map-locations';
 
+// Map modal parent class (will be extended)
 class MapModal {
   constructor(modalEl) {
     // modalEl must be set
-    if (!modalEl)
+    if (!modalEl) {
       return false;
+    }
 
+    // DOM elements
     this.modalEl = modalEl;
     this.mapEl = this.modalEl.querySelector('[data-map]');
-    if (this.mapEl) {
-      // init map
-      this.map = new window.google.maps.Map(this.mapEl, {
-        zoom: 15,
-        center: {lat: 50.128754, lng: 8.691756},
 
-        disableDefaultUI: true,
-      });
+    // map attributes
+    this.MAP_CENTER = {
+      lat: 50.128754,
+      lng: 8.691756,
+    };
+    this.initialZoom = 15;
 
+    this.markers = [];
+    this.infowindows = [];
+  }
 
-      // setup zoom listeners
-      window.google.maps.event.addDomListener(this.modalEl.querySelector('[data-map-zoom-in]'), 'click', () => {
-        this.map.setZoom(this.map.getZoom() + 1);
-      });
-      window.google.maps.event.addDomListener(this.modalEl.querySelector('[data-map-zoom-out]'), 'click', () => {
-        this.map.setZoom(this.map.getZoom() - 1);
-      });
+  init() {
+    this.initMap();
+    this.initZoomers();
+    this.initOverlay();
+    this.addMarkers();
+  }
 
+  initMap() {
+    this.map = new window.google.maps.Map(this.mapEl, {
+      zoom: this.initialZoom,
+      center: this.MAP_CENTER,
+      disableDefaultUI: true,
+    });
+  }
 
-      // overlay map
+  initOverlay() {
+    if (this.map) {
       let imageBounds = {
         north: 50.162275,
         south: 50.102031,
@@ -46,21 +47,178 @@ class MapModal {
         west: 8.622927,
       };
 
-      let travelOverlay = new google.maps.GroundOverlay(
+      new window.google.maps.GroundOverlay(
         'https://github.com/ugrupp/citygate/raw/master/src/images/layout/map-travel-bg.png',
         imageBounds,
         {
           opacity: 1,
           clickable: true,
-          map: this.map
+          map: this.map,
         }
       );
+    }
+  }
+
+  initZoomers() {
+    if (this.modalEl && this.map) {
+      // setup zoom listeners
+      window.google.maps.event.addDomListener(this.modalEl.querySelector('[data-map-zoom-in]'), 'click', () => {
+        this.map.setZoom(this.map.getZoom() + 1);
+      });
+      window.google.maps.event.addDomListener(this.modalEl.querySelector('[data-map-zoom-out]'), 'click', () => {
+        this.map.setZoom(this.map.getZoom() - 1);
+      });
+    }
+  }
+
+  // should be overridden by subclasses
+  addMarkers() {
+  }
+}
+
+// Travel (level 1)
+class TravelModal extends MapModal {
+  constructor(modalEl) {
+    super(modalEl);
+    this.init();
+  }
+
+  addMarkers() {
+    // citygate tower
+    this.addCitygateMarker();
+
+    // underground
+    this.addMarkersFactory({
+      icon: 'ubahn',
+      locationsKey: 'underground',
+    });
+
+    // tram
+    this.addMarkersFactory({
+      icon: 'tram',
+      locationsKey: 'tram',
+    });
+
+    // bus
+    this.addMarkersFactory({
+      icon: 'bus',
+      locationsKey: 'bus',
+    });
+
+    // S-Bahn
+    this.addMarkersFactory({
+      icon: 'tram',
+      locationsKey: 'sbahn',
+    });
+  }
+
+  // Citygate tower marker
+  addCitygateMarker() {
+    this.markers.push(new window.google.maps.Marker({
+      map: this.map,
+      position: this.MAP_CENTER,
+      icon: {
+        url: 'https://raw.githubusercontent.com/ugrupp/citygate/master/src/images/layout/markers/gebaeude-icon.png',
+        size: new window.google.maps.Size(64, 90),
+        anchor: new window.google.maps.Point(64, 90),
+      },
+    }));
+  }
+
+  // general factory method for adding standard markers (22x22px)
+  addMarkersFactory(options) {
+    let baseOptions = {
+      map: this.map,
+      icon: {
+        url: `https://raw.githubusercontent.com/ugrupp/citygate/master/src/images/layout/markers/${options.icon}.png`,
+        size: new window.google.maps.Size(22, 22),
+      },
     };
+
+    // loop through locations and add markers
+    LOCATIONS[options.locationsKey].forEach((markerOptions) => {
+      // prepare the infowindow
+      let infowindow = new window.google.maps.InfoWindow({
+        content: `
+          <article class="c-map-infowindow">
+            <h1>${markerOptions.name}</h1>
+            <div>${markerOptions.text || ''}</div>
+          </article>
+        `,
+        maxWidth: 300,
+      });
+
+      // add it to the infowindow list
+      this.infowindows.push(infowindow);
+
+      // finally set the marker and it to the markers list
+      let marker = new window.google.maps.Marker(Object.assign(baseOptions, {
+        position: markerOptions.coords,
+        title: markerOptions.name,
+      }));
+
+      this.markers.push(marker);
+
+      // set a click handler to open the corresponding infowindow
+      marker.addListener('click', () => {
+        // close other infowindows
+        this.infowindows.forEach(infowindow => infowindow.close());
+        // open infowindow
+        infowindow.open(this.map, marker);
+      });
+    });
+  }
+}
+
+// Around (level 2)
+class AroundModal extends MapModal {
+  constructor(modalEl) {
+    super(modalEl);
+
+    this.initialZoom = 17;
+    this.init();
+  }
+}
+
+// Campus mile (level 3)
+class CampusMileModal extends MapModal {
+  constructor(modalEl) {
+    super(modalEl);
+
+    this.initialZoom = 18;
+    this.init();
+  }
+}
+
+// Wrapper class around modals
+class MapModals {
+  constructor() {
+    if (typeof window.google !== 'undefined') {
+      this.modals = [...document.querySelectorAll('[data-map-modal]')];
+      if (this.modals.length) {
+        this.modals.forEach((modal) => {
+          // decide which modal type should be opened
+          switch (modal.dataset.mapModal) {
+          case 'travel':
+            new TravelModal(modal);
+            break;
+
+          case 'around':
+            new AroundModal(modal);
+            break;
+
+          case 'campusmile':
+            new CampusMileModal(modal);
+            break;
+          }
+        });
+      }
+    }
   }
 }
 
 // triggered by google script callback
 window.initMap = function() {
   new MapModals();
-}
+};
 
